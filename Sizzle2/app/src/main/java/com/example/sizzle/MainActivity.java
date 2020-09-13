e24ef476141bb718d6aff830e074787a90c390b3
An Doan <adoan@aithinktank.com>
2020-09-12 22:52:02 -0700

    Integrate web scraping.

--- Sizzle2/app/src/main/java/com/example/sizzle/MainActivity.java
+++ Sizzle2/app/src/main/java/com/example/sizzle/MainActivity.java
@@ -1,32 +1,30 @@
 package com.example.sizzle;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.graphics.Point;
 import android.graphics.Rect;
 import android.os.Bundle;
-import android.graphics.Bitmap;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
 import com.google.mlkit.vision.common.InputImage;
 import com.google.mlkit.vision.text.Text;
 import com.google.mlkit.vision.text.TextRecognition;
 import com.google.mlkit.vision.text.TextRecognizer;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-
-import android.provider.MediaStore;
-import android.view.View;
-import android.widget.Button;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.widget.ImageView;
+import java.net.URLEncoder;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -58,13 +56,19 @@ public class MainActivity extends AppCompatActivity {
         InputImage image = InputImage.fromBitmap(photo, 0); // Only 0, 90, 180, 270 are supported
         imageView.setImageBitmap(photo);
         String recognizedText = recognizeText(image);
+        System.out.println(recognizedText);
         // DO WHATEVER YOU WANT WITH THIS TEXT!!
+        try {
+            getUrlAndCovidProcedures(recognizedText);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     private String recognizeText(InputImage image) {
 
         TextRecognizer recognizer = TextRecognition.getClient();
-        final String[] recognizedText = {""};
+        final String[] recognizedText = {"No text detected :("};
 
         Task<Text> result =
                 recognizer.process(image)
@@ -96,6 +100,49 @@ public class MainActivity extends AppCompatActivity {
                                     }
                                 });
         return recognizedText[0];
+    }
+
+
+    public static void getUrlAndCovidProcedures(String text) throws Exception {
+
+        final String USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.89 Safari/537.36";
+        String query = text + "near me yelp";
+        final Document page = Jsoup.connect("https://www.google.com/search?q=" + URLEncoder.encode(query, "UTF-8")).userAgent(USER_AGENT).get();
+        //Traverse the results
+        boolean yelpFinished = false;
+        for (Element result : page.select("div > div > div > div > a")){
+
+
+            final String url = result.attr("href");
+            if(!url.contains("google") && !url.contains("search") && !url.contains("#")) {
+                System.out.println(url);
+            }
+            if(url.contains("yelp.ca") && yelpFinished == false) {
+                yelpFinished = true;
+                System.out.println(url);
+                final Document yelpPage = Jsoup.connect(url).userAgent(USER_AGENT).get();
+
+                Element result2 = yelpPage.select("section").first();
+
+                for (Element result3 : result2.select("div > div > div")) {
+                    //System.out.println(result3);
+                    String fullProcedures = "";
+                    for (Element procedures : result3.select("div > div > span")) {
+
+                        fullProcedures += procedures.text() + " ";
+
+                    }
+
+                    fullProcedures = fullProcedures.replaceAll("\n[ \t]*\n", "\n");
+                    System.out.print(fullProcedures);
+
+                }
+
+            }
+
+
+        }
+
     }
 
     // For string processing in case you feel as if the total text needs to be processed more !
